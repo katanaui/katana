49aee2aef915fc4e912c348dff7c6cb147ec22d2
Tony Lea <tnylea@gmail.com>
2025-06-27 15:33:58 -0400

    Adding update to component

--- resources/views/components/katana/modal/fullscreen.blade.php
+++ resources/views/components/katana/modal/fullscreen.blade.php
@@ -1,6 +1,12 @@
 <div x-data="{ 
         open: false 
     }"
+    x-init="
+    console.log('made');
+        $watch('open', function(value){
+            console.log('cahnged: ' + value);
+        });
+    "
     class="relative w-auto h-auto">
     <div @click="open=true">
     {{ $trigger }}
@@ -15,8 +21,7 @@
                 <div class="overflow-hidden absolute inset-0">
                     <div class="flex fixed inset-y-0 right-0 max-w-full">
                         <div 
-                            x-show="open" 
-                            @click.away="open = false"
+                            x-show="open"
                             x-transition:enter="transform transition ease-in-out duration-300" 
                             x-transition:enter-start="scale-[0.95] opacity-0" 
                             x-transition:enter-end="scale-1 opacity-100" 
@@ -24,7 +29,7 @@
                             x-transition:leave-start="scale-1 opacity-100" 
                             x-transition:leave-end="scale-[0.99] opacity-0" 
                             class="w-screen max-w-full">
-                            <div class="flex overflow-y-scroll flex-col pb-5 h-full bg-white border-l shadow-lg border-neutral-100/70">
+                            <div class="flex overflow-y-scroll flex-col h-full bg-white border-l shadow-lg border-neutral-100/70">
                                 <div class="flex flex-shrink-0 justify-center items-center w-full h-16">
                                     {{ $title }}
                                 </div>
