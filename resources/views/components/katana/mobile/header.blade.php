be09d8ccf7b1ba1d434a0360618cf26da1bb9e60
Tony Lea <tnylea@gmail.com>
2025-06-29 15:08:56 -0400

    Adding additional component

---
+++ resources/views/components/katana/mobile/header.blade.php
@@ -0,0 +1,27 @@
+@props([
+    'title' => '',
+    'leftIcon' => '',
+    'leftUrl' => '',
+    'rightIcon' => '',
+    'rightUrl' => ''
+])
+
+<div class="flex relative justify-start items-center px-3 w-full h-16 border-b border-gray-200">
+    @if($leftIcon && $leftUrl)
+        <a href="{{ $leftUrl ?? '' }}" class="relative z-10 p-2">
+            <x-dynamic-component :component="$leftIcon" class="w-5 h-5" />
+        </a>
+    @endif
+    @if($leftSlot ?? false)
+        {{ $leftSlot }}
+    @endif
+    <h1 class="flex absolute inset-0 z-0 justify-center items-center w-full text-lg font-semibold text-gray-900">{{ $title ?? '' }}</h1>
+    @if($rightSlot ?? false)
+        {{ $rightSlot }}
+    @endif
+    @if($rightIcon && $rightUrl)
+        <a href="{{ $rightUrl ?? '' }}" class="relative z-10 p-2">
+            <x-dynamic-component :component="$rightIcon" class="w-5 h-5" />
+        </a>
+    @endif
+</div>
