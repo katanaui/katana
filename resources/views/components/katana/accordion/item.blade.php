6c2a73436abae49e9107116da40d979e769edb94
tnylea <tonylea@macbookair.local>
2025-09-17 13:16:14 -0400

    Adding latest updates

--- resources/views/components/katana/accordion/item.blade.php
+++ resources/views/components/katana/accordion/item.blade.php
@@ -20,22 +20,26 @@
             }
         }
     "
-    class="group"
+    {{ $attributes->twMerge('group') }}
 >
     <button 
         @click="toggle(id)" 
-        class="flex w-full items-center justify-between py-4 px-3 text-left select-none font-medium"
+        {{ $attributes->twMergeFor('title', 'flex w-full items-center text-foreground/80 group-hover:text-foreground justify-between py-4 px-3 text-left select-none font-medium group-hover:underline') }}
     >
         <span>{{ $title }}</span>
-        <span class="w-4 h-4 duration-200 ease-out transform" :class="{ 'rotate-180': isOpen(id) }">
-            <x-phosphor-caret-down class="w-full h-full" />
+        <span  {{ $attributes->twMergeFor('icon', 'w-4 h-4 duration-200 ease-out transform') }} :class="{ 'rotate-180': isOpen(id) }">
+            @if($icon ?? false)
+                {!! $icon !!}
+            @else
+                <x-phosphor-caret-down class="w-full h-full" />
+            @endif
         </span>
     </button>
     <div 
         x-show="isOpen(id)" 
         x-collapse 
         x-cloak
-        class="px-3 pb-4 pt-0 text-sm text-foreground/70"
+        {{ $attributes->twMergeFor('content', 'px-3 pb-4 pt-0 text-sm text-foreground/70') }}
     >
         {{ $slot }}
     </div>
