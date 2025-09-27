c8ca6763316e9bf45174efa056d7b93e87ce73ac
tnylea <tonylea@macbookair.local>
2025-09-27 16:43:56 -0400

    Adding the checkbox component

--- resources/views/components/katana/checkbox.blade.php
+++ resources/views/components/katana/checkbox.blade.php
@@ -4,7 +4,7 @@
     'checked' => false,
     'disabled' => false,
     'clickable' => false,
-    'checkmarkStrokeWidth' => 3,
+    'icon' => null,
     'hideCheckbox' => false
 ])
 
@@ -21,7 +21,7 @@
     $innerLabelAttribute = $clickable ? 'span' : 'label for="' . $id . '"';
 @endphp
 
-<{!! $containerAttribute !!} x-data="{ checked: {{ $checked ? 'true' : 'false' }} }" x-bind:data-checked="checked ? true : null" {{ $attributes->twMerge($classes) }}>
+<{!! $containerAttribute !!} x-data="{ checked: {{ $checked ? 'true' : 'false' }} }" x-init="const checkFunction = function(){ checked = true };$refs.checkbox.check = checkFunction;const unCheckFunction = function(){ checked = false };$refs.checkbox.uncheck = unCheckFunction;" x-bind:data-checked="checked ? true : null" {{ $attributes->only(['class'])->twMerge($classes) }}>
     <input
         type="checkbox"
         id="{{ $id }}"
@@ -29,16 +29,21 @@
         x-bind:aria-checked="checked"
         x-on:change="checked = $event.target.checked"
         x-ref="checkbox"
+        @check="checked = true"
+        @uncheck="checked = false"
         @checked($checked)
         @disabled($disabled)
-        {{ $attributes->withoutTwMergeClasses() }}
+        {{ $attributes->withoutTwMergeClasses()->except(['class']) }}
     />
 
     @if(!$hideCheckbox)
         <span @if(!$clickable && !$disabled) @click="$refs.checkbox.checked = !$refs.checkbox.checked; $refs.checkbox.dispatchEvent(new Event('change')); $refs.checkbox.focus()" @endif 
-            {{ $attributes->twMergeFor('checkbox', 'w-5 h-5 border border-gray-300 rounded-md flex items-center justify-center transition group-data-checked:border-primary group-data-checked:bg-primary peer-disabled:opacity-60 peer-focus:ring-2 peer-focus:ring-primary/30') }}>
-            <!-- Checkmark (only visible when checked) -->
-            <svg class="w-3.5 h-3.5 stroke-current duration-150 text-primary-foreground ease group-data-checked:opacity-100 opacity-0 group-data-checked:scale-100 scale-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="{{ $checkmarkStrokeWidth }}"><path d="M20 6 9 17l-5-5"/></svg>
+            {{ $attributes->twMergeFor('checkbox', 'w-5 h-5 border border-gray-300 rounded-md flex items-center justify-center transition group-data-checked:border-primary group-data-checked:bg-primary peer-disabled:opacity-60 peer-focus:ring-2 peer-focus:ring-primary/20') }}>
+            @if($icon)
+                <x-dynamic-component :component="$icon" {{ $attributes->twMergeFor('checkmark', 'w-3.5 h-3.5 stroke-current duration-150 text-primary-foreground ease group-data-checked:opacity-100 opacity-0 group-data-checked:scale-100 scale-50') }} />
+            @else
+                <svg {{ $attributes->twMergeFor('checkmark', 'w-3.5 h-3.5 stroke-current duration-150 text-primary-foreground ease group-data-checked:opacity-100 opacity-0 group-data-checked:scale-100 scale-50') }} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3"><path d="M20 6 9 17l-5-5"/></svg>
+            @endif
         </span>
     @endif
 
