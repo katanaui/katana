eee0f1e52f600300609bac1d2371d891a5b7726b
tnylea <tonylea@macbookair.local>
2026-01-01 15:33:34 -0500

    Adding latest

--- resources/views/components/katana/accordion/item.blade.php
+++ resources/views/components/katana/accordion/item.blade.php
@@ -20,18 +20,18 @@
             }
         }
     "
-    {{ $attributes->twMerge('group') }}
+    {{ $attributes->twMerge('group/accordion-item') }}
 >
     <button 
         @click="toggle(id)" 
-        {{ $attributes->twMergeFor('title', 'flex w-full items-center text-foreground/80 group-hover:text-foreground justify-between py-4 px-3 text-left select-none font-medium group-hover:underline') }}
+        {{ $attributes->twMergeFor('title', 'flex w-full items-center text-foreground/80 group-hover/accordion-item:text-foreground justify-between py-4 px-3 text-left select-none font-medium group-hover/accordion-item:underline') }}
     >
         <span>{{ $title }}</span>
-        <span  {{ $attributes->twMergeFor('icon', 'w-4 h-4 duration-200 ease-out transform') }} :class="{ 'rotate-180': isOpen(id) }">
+        <span  {{ $attributes->twMergeFor('icon', 'w-3.5 h-3.5 duration-200 ease-out transform') }} :class="{ 'rotate-180': isOpen(id) }">
             @if($icon ?? false)
                 {!! $icon !!}
             @else
-                <x-phosphor-caret-down class="w-full h-full" />
+                <svg class="w-full h-full" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256"><path fill="none" d="M0 0h256v256H0z"/><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="24" d="m208 96-80 80-80-80"/></svg>
             @endif
         </span>
     </button>
