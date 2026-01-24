3947e10305c8fe1e6e94001dcbdde58f1dd67489
Tony Lea <tnylea@gmail.com>
2026-01-24 15:04:29 -0500

    fixing the toast notification

--- resources/views/components/katana/toast.blade.php
+++ resources/views/components/katana/toast.blade.php
@@ -1,11 +1,11 @@
 <template x-teleport="body">
-    <div {{ $attributes->twMerge('fixed top-0 sm:px-2 px-1 pointer-events-none pb-4 left-1/2 -translate-x-1/2 w-screen h-auto sm:max-w-sm z-[99999999]') }}
+    <div {{ $attributes->twMerge('fixed top-0 sm:px-2 px-1 pointer-events-none pb-4 left-1/2 -translate-x-1/2 w-full sm:w-screen h-auto sm:max-w-sm z-[99999999]') }}
         x-data="{
             toasts: [],
             toastsProgress: [],
             toast: @js(session('toast')),
             type: ['success', 'error', 'warning', 'info', 'blank'],
-            closeInterval: 5000,
+            closeInterval: 500000,
             addToast(message, type, description = '') {
                 const id = Date.now() + Math.random();
                 const toast = { id, type, message, description, startTime: null, rafId: null, pausedAt: null, totalPausedTime: 0 };
@@ -71,6 +71,9 @@
                 'exclamation-circle': `<svg xmlns='http://www.w3.org/2000/svg' class='w-full h-full' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'><circle cx='12' cy='12' r='10'/><line x1='12' x2='12' y1='8' y2='12'/><line x1='12' x2='12.01' y1='16' y2='16'/></svg>`,
                 'exclamation-triangle': `<svg xmlns='http://www.w3.org/2000/svg' class='w-full h-full' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'><path d='M7.9 20A9 9 0 1 0 4 16.1L2 22Z'/><path d='M12 8v4'/><path d='M12 16h.01'/></svg>`,
                 'information-circle': `<svg xmlns='http://www.w3.org/2000/svg' class='w-full h-full' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'><path d='M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z'/><path d='M13 8H7'/><path d='M17 12H7'/></svg>`
+            },
+            get isTouchDevice() {
+                return 'ontouchstart' in window || navigator.maxTouchPoints > 0;
             }
         }" x-init="
             @if(session('toast'))
@@ -92,9 +95,9 @@
         <div class="mt-1 sm:mt-2 space-y-1 sm:space-y-2">
             <template x-for="toast in toasts" :key="toast.id">
                 <div :id="'katana-toast-' + toast.id" x-data popover="manual"
-                    @mouseenter="pauseToast(toast.id)"
-                    @mouseleave="resumeToast(toast.id)"
-                    class="flex overflow-hidden relative top-0 pointer-events-auto left-1/2 -translate-x-1/2 flex-col items-start p-3.5 px-5 w-full sm:max-w-sm text-sm text-white rounded sm:rounded-2xl opacity-100 duration-300 ease-out translate-y-0 starting:opacity-0 starting:-translate-y-full ending:-translate-y-full ending:opacity-0 backdrop-blur-xs group bg-black/60 dark:border dark:border-accent"
+                    @mouseenter="if (!isTouchDevice) pauseToast(toast.id)"
+                    @mouseleave="if (!isTouchDevice) resumeToast(toast.id)"
+                    class="flex overflow-hidden relative top-0 pointer-events-auto left-1/2 -translate-x-1/2 flex-col items-start p-3.5 px-5 w-full sm:max-w-sm text-sm text-white rounded-md sm:rounded-2xl opacity-100 duration-300 ease-out translate-y-0 starting:opacity-0 starting:-translate-y-full ending:-translate-y-full ending:opacity-0 backdrop-blur-xs group bg-black/60 dark:border dark:border-accent"
                     role="alert">
                     <!-- Progress Bar -->
                     <div class="absolute inset-0 z-10 h-full duration-300 ease-linear bg-black/70 dark:bg-white/10"
@@ -104,7 +107,7 @@
                             x-html="icons[types[toast.type].icon]"></span>
                         <span x-text="toast.message"></span>
                         <span x-on:click="removeToast(toast.id)"
-                            class="flex absolute right-0 top-1/2 justify-center items-center translate-x-1.5 w-6 h-6 rounded-lg opacity-0 duration-100 ease-out scale-50 -translate-y-1/2 cursor-pointer group-hover:scale-100 group-hover:opacity-50 group-hover:hover:opacity-100 group-hover:text-white hover:opacity-100 bg-black/50 hover:dark:bg-white/10"
+                            class="flex absolute right-0 top-1/2 justify-center items-center translate-x-1.5 w-6 h-6 rounded-lg sm:opacity-0 duration-100 ease-out sm:scale-50 -translate-y-1/2 cursor-pointer group-hover:scale-100 group-hover:opacity-50 group-hover:hover:opacity-100 group-hover:text-white hover:opacity-100 bg-black/50 hover:dark:bg-white/10"
                             :class="{ '-mt-1 -mr-1': toast.description != '' }">
                             <svg xmlns="http://www.w3.org/2000/svg" class="w-3.5 h-3.5" viewBox="0 0 24 24" fill="none"
                                 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
