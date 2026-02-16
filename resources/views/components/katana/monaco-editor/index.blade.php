3656f08695b1750bad15abe12d73044e57915e4e
tnylea <tonylea@Mac.lan>
2026-02-16 16:08:11 -0500

    Adding fix for monaco editor, working version

--- resources/views/components/katana/monaco-editor/index.blade.php
+++ resources/views/components/katana/monaco-editor/index.blade.php
@@ -187,6 +187,13 @@
                 return Promise.resolve();
             }
             return new Promise((resolve, reject) => {
+                if (!window._monacoEditorCssLoaded) {
+                    window._monacoEditorCssLoaded = true;
+                    const link = document.createElement('link');
+                    link.rel = 'stylesheet';
+                    link.href = '{{ asset('katana/monaco-editor.css') }}';
+                    document.head.appendChild(link);
+                }
                 const script = document.createElement('script');
                 script.src = '{{ asset('katana/monaco-editor.js') }}';
                 script.onload = () => resolve();
@@ -209,6 +216,7 @@
         }); --}}
 
         {{-- Make sure that loadScript() is complete --}}
+        window.monacoInstances = window.monacoInstances || {};
         loadScript().then(() => {
 
             let lineNumberAttributes = {
@@ -223,8 +231,17 @@
                     }
                 @endif
 
-            let proxy = URL.createObjectURL(new Blob([` self.MonacoEnvironment = { baseUrl: 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.39.0/min' }; importScripts('https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.39.0/min/vs/base/worker/workerMain.min.js');`], { type: 'text/javascript' }));
-            window.MonacoEnvironment = { getWorkerUrl: () => proxy };
+            if (!window.MonacoEnvironment) {
+                window.MonacoEnvironment = {
+                    getWorker: function(workerId, label) {
+                        const workerBlob = new Blob([
+                            'self.MonacoEnvironment = { baseUrl: `https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.52.0/min/` };' +
+                            'importScripts(`https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.52.0/min/vs/base/worker/workerMain.js`);'
+                        ], { type: 'text/javascript' });
+                        return new Worker(URL.createObjectURL(workerBlob));
+                    }
+                };
+            }
 
                 monacoTheme = {'base':'vs-dark','inherit':true,'rules':[{'background':'000000','token':''},{'foreground':'aeaeae','token':'comment'},{'foreground':'d8fa3c','token':'constant'},{'foreground':'ff6400','token':'entity'},{'foreground':'fbde2d','token':'keyword'},{'foreground':'fbde2d','token':'storage'},{'foreground':'61ce3c','token':'string'},{'foreground':'61ce3c','token':'meta.verbatim'},{'foreground':'8da6ce','token':'support'},{'foreground':'ab2a1d','fontStyle':'italic','token':'invalid.deprecated'},{'foreground':'f8f8f8','background':'9d1e15','token':'invalid.illegal'},{'foreground':'ff6400','fontStyle':'italic','token':'entity.other.inherited-class'},{'foreground':'ff6400','token':'string constant.other.placeholder'},{'foreground':'becde6','token':'meta.function-call.py'},{'foreground':'7f90aa','token':'meta.tag'},{'foreground':'7f90aa','token':'meta.tag entity'},{'foreground':'ffffff','token':'entity.name.section'},{'foreground':'d5e0f3','token':'keyword.type.variant'},{'foreground':'f8f8f8','token':'source.ocaml keyword.operator.symbol'},{'foreground':'8da6ce','token':'source.ocaml keyword.operator.symbol.infix'},{'foreground':'8da6ce','token':'source.ocaml keyword.operator.symbol.prefix'},{'fontStyle':'underline','token':'source.ocaml keyword.operator.symbol.infix.floating-point'},{'fontStyle':'underline','token':'source.ocaml keyword.operator.symbol.prefix.floating-point'},{'fontStyle':'underline','token':'source.ocaml constant.numeric.floating-point'},{'background':'ffffff08','token':'text.tex.latex meta.function.environment'},{'background':'7a96fa08','token':'text.tex.latex meta.function.environment meta.function.environment'},{'foreground':'fbde2d','token':'text.tex.latex support.function'},{'foreground':'ffffff','token':'source.plist string.unquoted'},{'foreground':'ffffff','token':'source.plist keyword.operator'}],'colors':{'editor.foreground':'#F8F8F8','editor.background':'#000000','editor.selectionBackground':'#253B76','editor.lineHighlightBackground':'#FFFFFF0F','editorCursor.foreground':'#FFFFFFA6','editorWhitespace.foreground':'#FFFFFF40'}};
                 monacoThemeLight = {'base':'vs','inherit':true,'rules':[{'background':'ffffff','token':''},{'foreground':'6a737d','token':'comment'},{'foreground':'005cc5','token':'constant'},{'foreground':'e36209','token':'entity'},{'foreground':'d73a49','token':'keyword'},{'foreground':'d73a49','token':'storage'},{'foreground':'032f62','token':'string'},{'foreground':'032f62','token':'meta.verbatim'},{'foreground':'005cc5','token':'support'},{'foreground':'b31d28','fontStyle':'italic','token':'invalid.deprecated'},{'foreground':'b31d28','background':'ffeef0','token':'invalid.illegal'},{'foreground':'6f42c1','fontStyle':'italic','token':'entity.other.inherited-class'},{'foreground':'22863a','token':'string constant.other.placeholder'},{'foreground':'005cc5','token':'meta.function-call.py'},{'foreground':'22863a','token':'meta.tag'},{'foreground':'22863a','token':'meta.tag entity'},{'foreground':'24292e','token':'entity.name.section'},{'foreground':'24292e','token':'keyword.type.variant'},{'foreground':'24292e','token':'source.ocaml keyword.operator.symbol'},{'foreground':'005cc5','token':'source.ocaml keyword.operator.symbol.infix'},{'foreground':'005cc5','token':'source.ocaml keyword.operator.symbol.prefix'},{'fontStyle':'underline','token':'source.ocaml keyword.operator.symbol.infix.floating-point'},{'fontStyle':'underline','token':'source.ocaml keyword.operator.symbol.prefix.floating-point'},{'fontStyle':'underline','token':'source.ocaml constant.numeric.floating-point'},{'background':'f6f8f8','token':'text.tex.latex meta.function.environment'},{'background':'f6f8f8','token':'text.tex.latex meta.function.environment meta.function.environment'},{'foreground':'d73a49','token':'text.tex.latex support.function'},{'foreground':'24292e','token':'source.plist string.unquoted'},{'foreground':'24292e','token':'source.plist keyword.operator'}],'colors':{'editor.foreground':'#24292e','editor.background':'#ffffff','editor.selectionBackground':'#c8c8fa','editor.lineHighlightBackground':'#f5f5f6','editorCursor.foreground':'#24292e','editorWhitespace.foreground':'#e1e4e8'}};
