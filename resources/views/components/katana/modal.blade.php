274f6815ca43b90219ddf4084881847a408eccea
Tony Lea <tony@devdojo.com>
2026-03-08 17:42:34 -0400

    updates to modal

--- resources/views/components/katana/modal.blade.php
+++ resources/views/components/katana/modal.blade.php
@@ -4,19 +4,13 @@
     'zIndex' => 50,
 ])
 
-<div x-data="{ modalOpen: false }" 
-    x-trap="modalOpen"
-    class="relative w-auto h-auto" 
-    @keydown.escape.window="modalOpen = false"
-    x-init=" 
-        $watch('modalOpen', function(value) {
-            if(value) {
-                document.body.style.overflow = 'hidden';
-            } else {
-                document.body.style.overflow = 'auto';
-            }
-        });
-    ">
+<div x-data="{ modalOpen: false }" x-trap="modalOpen" x-init=" $watch('modalOpen', function(value) {
+     if (value) {
+         document.body.style.overflow = 'hidden';
+     } else {
+         document.body.style.overflow = 'auto';
+     }
+ });" class="relative h-auto w-auto" @keydown.escape.window="modalOpen = false">
     <div @click="modalOpen=true">
         @if (empty(trim($slot)))
             <x-button variant="outline">Open</x-button>
@@ -25,25 +19,20 @@
         @endif
     </div>
     <template x-teleport="body">
-        <div x-show="modalOpen" x-cloak 
-            @open-modal.window="if($event.detail.id === $el.id) modalOpen=true"
-            @close-modal.window="if($event.detail.id === $el.id) modalOpen=false"
-            x-init="console.log('Modal component initialized with id:', $el.id);"
-            {{ $attributes->withoutTwMergeClasses()->only('id') }}
-            class="fixed left-0 top-0 flex h-dvh w-screen items-center justify-center" style="z-index: {{ $zIndex }}">
-            <div class="absolute inset-0 w-full h-full bg-black/60" x-show="modalOpen" x-transition:enter="ease-out duration-300" x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100" x-transition:leave="ease-in duration-300" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0" @click="modalOpen=false"></div>
-            <div x-show="modalOpen" x-trap.inert.noscroll="modalOpen" x-transition:enter="ease-out duration-300" x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100" x-transition:leave="ease-in duration-200" x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100" x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" class="flex justify-center items-center w-full h-full">
-                <div {{  $attributes->twMerge('relative px-7 py-6 w-full dark:border dark:border-accent bg-background text-foreground sm:max-w-lg sm:rounded-medium') }}>
-                    @if(isset($header))
+        <div x-show="modalOpen" x-cloak x-init="console.log('Modal component initialized with id:', $el.id);" class="fixed left-0 top-0 flex h-dvh w-screen items-center justify-center" @open-modal.window="if($event.detail.id === $el.id) modalOpen=true" @close-modal.window="if($event.detail.id === $el.id) modalOpen=false" {{ $attributes->withoutTwMergeClasses()->only('id') }} style="z-index: {{ $zIndex }}">
+            <div x-show="modalOpen" x-transition:enter="ease-out duration-300" x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100" x-transition:leave="ease-in duration-300" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0" class="absolute inset-0 h-full w-full bg-black/60" @click="modalOpen=false"></div>
+            <div x-show="modalOpen" x-trap.inert.noscroll="modalOpen" x-transition:enter="ease-out duration-300" x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100" x-transition:leave="ease-in duration-200" x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100" x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" class="flex h-full w-full items-center justify-center">
+                <div {{ $attributes->twMerge('relative px-7 py-6 w-full dark:border dark:border-accent bg-background text-foreground sm:max-w-lg sm:rounded-medium') }}>
+                    @if (isset($header))
                         <h2 {{ $attributes->twMergeFor('header', '-translate-y-1.5 text-lg mb-2') }}>{{ $header }}</h2>
                     @endif
                     {{ $content ?? '' }}
-                    @if(isset($footer))
+                    @if (isset($footer))
                         <div {{ $attributes->twMergeFor('footer', 'w-full flex items-center justify-end gap-2 translate-y-1.5 mt-5') }}>{{ $footer }}</div>
                     @endif
                     @if ($close_button ?? true)
-                        <div class="hidden absolute top-0 right-0 pt-4 pr-4 sm:block z-50">
-                            <button class="text-foreground/30 p-1 rounded-md hover:bg-accent bg-transparent hover:text-foreground/50 focus-visible:ring-2 focus-visible:ring-foreground/20 focus:outline-none" @click="modalOpen=false" type="button" aria-expanded="true">
+                        <div class="absolute right-0 top-0 z-50 hidden pr-4 pt-4 sm:block">
+                            <button class="text-foreground/30 hover:bg-accent hover:text-foreground/50 focus-visible:ring-foreground/20 rounded-md bg-transparent p-1 focus:outline-none focus-visible:ring-2" @click="modalOpen=false" type="button" aria-expanded="true">
                                 <span class="sr-only">Close</span>
                                 <svg class="size-5" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" data-slot="icon" aria-hidden="true">
                                     <path d="M6 18 18 6M6 6l12 12" stroke-linecap="round" stroke-linejoin="round"></path>
